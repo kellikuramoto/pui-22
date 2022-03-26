964b0577fd30446e3066ffb7304a7878d4efe036
kellikuramoto <kkuramot@andrew.cmu.edu>
2022-03-26 17:00:26 -0400

    added carousel to all individual product pages

--- homework_6b/main.js
+++ homework_6b/main.js
@@ -493,7 +493,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1} 
   x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 2000); 
+  setTimeout(carousel, 2200); 
 }
 
 // @purpose updates the Cart page when user removes products
