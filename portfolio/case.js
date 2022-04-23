93984453b696c51694287ac5ace0f0add788bb2d
kellikuramoto <kkuramot@andrew.cmu.edu>
2022-04-23 14:59:56 -0400

    Selling Later Case Study

--- portfolio/case.js
+++ portfolio/case.js
@@ -59,3 +59,58 @@ function loadCaseInfo() {
 
 	document.getElementById("cover").setAttribute('src', projects[currentCase].cover);
 }
+
+let currentCase = localStorage.getItem('Case');
+let div = document.getElementById('process');
+
+div.addEventListener('scroll', function() {
+	let currentCase = localStorage.getItem('Case');
+
+	var element = document.querySelector('#current-state');
+	var position = element.getBoundingClientRect();
+
+	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+		document.getElementById('current-state-link').style.color = projects[currentCase].background;
+		document.getElementById('final-prototype-link').style.fontWeight = '600';
+	}
+});
+
+div.addEventListener('scroll', function() {
+	var element = document.querySelector('#iteration-testing');
+	var position = element.getBoundingClientRect();
+
+	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+		document.getElementById('iteration-testing-link').style.color = projects[currentCase].background;
+		document.getElementById('final-prototype-link').style.fontWeight = '600';
+	}
+});
+
+div.addEventListener('scroll', function() {
+	var element = document.querySelector('#problems-solutions');
+	var position = element.getBoundingClientRect();
+
+	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+		document.getElementById('problems-solutions-link').style.color = projects[currentCase].background;
+		document.getElementById('final-prototype-link').style.fontWeight = '600';
+	}
+});
+
+div.addEventListener('scroll', function() {
+	var element = document.querySelector('#final-prototype');
+	var position = element.getBoundingClientRect();
+
+	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+		document.getElementById('final-prototype-link').style.color = projects[currentCase].background;
+		document.getElementById('final-prototype-link').style.fontWeight = '600';
+	}
+});
+
+div.addEventListener('scroll', function() {
+	var element = document.querySelector('#conclusion');
+	var position = element.getBoundingClientRect();
+
+	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+		document.getElementById('conclusion-link').style.color = projects[currentCase].background;
+		document.getElementById('final-prototype-link').style.fontWeight = '600';
+	}
+});
